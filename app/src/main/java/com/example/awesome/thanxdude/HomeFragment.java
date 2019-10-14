009b3a91cdfac05584a183c27034c833ee51f5d5
hp <piyushpawar199954@gmail.com>
2019-10-14 13:54:18 +0530

    upto blog-list_item frontend (error free)

--- app/src/main/java/com/example/awesome/thanxdude/HomeFragment.java
+++ app/src/main/java/com/example/awesome/thanxdude/HomeFragment.java
@@ -4,6 +4,7 @@ package com.example.awesome.thanxdude;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private RecyclerView blog_list_view;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,6 +27,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        blog_list_view = getActivity().findViewById(R.id.blog_list_view);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
